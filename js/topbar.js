e67d88b79a9a0d23743c1dca3232bbf48b32c3b8
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2018-04-07 23:48:25 +0200

    Fixed TypeError where there was no filename

--- js/topbar.js
+++ js/topbar.js
@@ -1,10 +1,18 @@
-var file = document.location.pathname.match(/[^\/]+$/)[0] //Name of the file
-var buttons = document.getElementsByClassName("button");
+try {
+    var file = document.location.pathname.match(/[^\/]+$/)[0] //Name of the file
+} 
+catch (TypeError) { //In case It doesn't find the name of the file
+    var file = 'index.php'
+}
 
+var buttons = document.getElementsByClassName("button");
 for (let i = 0; i < buttons.length; i++) {
-    var id = "button"+i
-    var button = document.getElementById(id).getAttribute("href");
-    if (button == file) {
-        document.getElementById(id).classList.add("active")
-    }
+    var id = "button"+i;
+
+        var button = document.getElementById(id).getAttribute("href");
+    
+        if (button == file) {
+            document.getElementById(id).classList.add("active")
+        }
+
 }
