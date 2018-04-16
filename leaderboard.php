19337ad6707a8c201642dfad7a98a03d08938d63
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2018-04-16 11:46:20 +0200

    Table....

--- leaderboard.php
+++ leaderboard.php
@@ -25,7 +25,44 @@
     ?>
 
         <div class="content">
-
+            <table>
+                <tr>
+                    <th>Position</th>
+                    <th>Club</th>
+                    <th>Played</th>
+                    <th>Won</th>
+                    <th>Drawn</th>
+                    <th>Lost</th>
+                    <th>GF</th>
+                    <th>GA</th>
+                    <th>GD</th>
+                    <th>Points</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Next Generation</td>
+                    <td>30</td>
+                    <td>29</td>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>69</td>
+                    <td>5</td>
+                    <td>+63</td>
+                    <td>88</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Next Generation</td>
+                    <td>30</td>
+                    <td>29</td>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>69</td>
+                    <td>5</td>
+                    <td>+63</td>
+                    <td>88</td>
+                </tr>
+            </table>
         
         </div>
         <?php include('footer.php'); ?>
